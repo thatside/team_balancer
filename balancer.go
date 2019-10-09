a20d7a45991bc4ce6d36bf1435b3b599371fa336
thatside <harammin@gmail.com>
2019-10-10 01:47:15 +0300

    Some fixes and tests

--- balancer.go
+++ balancer.go
@@ -1,10 +1,25 @@
 package main
 
-import "math"
+import (
+	"math"
+	"math/rand"
+	"strconv"
+	"time"
+)
 
 type Player struct {
 	Name string
 	Rank uint8
+}
+
+func CreatePlayer(rank uint8) *Player {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	return &Player{
+		Name: "Player" + strconv.Itoa(r1.Intn(100)),
+		Rank: rank,
+	}
 }
 
 type Team struct {
@@ -21,6 +36,25 @@ type MatchRoster struct {
 	RankRange uint8
 }
 
+func CreateEmptyRoster(teamSize, rankRange uint8) *MatchRoster {
+	var playersA, playersB []Player
+	roster := &MatchRoster{
+		TeamA: &Team{Players: &playersA},
+		TeamB: &Team{Players: &playersB},
+	}
+
+	roster.CurrentTeam = roster.TeamA
+	roster.OppositeTeam = roster.TeamB
+	roster.TeamSize = teamSize
+	roster.RankRange = rankRange 
+	
+	return roster
+}
+
+func (t *Team) AddPlayer(player *Player) {
+	*t.Players = append(*t.Players, *player)
+}
+
 type BalancesTeams interface {
 	Init(teamSize, rankRange uint8)
 	AddPlayer(player Player) (addedToTeam bool)
@@ -34,17 +68,8 @@ type TeamBalancer struct {
 }
 
 func (tb *TeamBalancer) Init(teamSize, rankRange uint8) {
-	var playersA, playersB []Player
-	roster := &MatchRoster{
-		TeamA: &Team{Players: &playersA},
-		TeamB: &Team{Players: &playersB},
-	}
-
-	roster.CurrentTeam = roster.TeamA
-	roster.OppositeTeam = roster.TeamB
-	roster.TeamSize = teamSize
-	roster.RankRange = rankRange
-
+	roster := CreateEmptyRoster(teamSize, rankRange)
+	
 	tb.insertChan = make(chan Player)
 	tb.insertedChan = make(chan bool)
 	tb.rosterChan = make(chan *MatchRoster)
@@ -62,6 +87,9 @@ func (tb *TeamBalancer) AddPlayer(player Player) (addedToTeam, finishedBalancing
 		return inserted, false
 	case roster := <- tb.rosterChan:
 		tb.Roster = roster
+		close(tb.insertedChan)
+		close(tb.insertChan)
+		close(tb.rosterChan)
 		return false, true
 	}
 }
@@ -71,41 +99,46 @@ func (tb *TeamBalancer) runBalancing() {
 		for {
 			player := <-insertChan
 
-			currentTeam := roster.CurrentTeam
-			oppositeTeam := roster.OppositeTeam
+			inserted, finished := DoBalance(roster, player)
 
-			if len(*currentTeam.Players) == 0 && len(*oppositeTeam.Players) == 0 {
-				currentTeam.BasicRank = player.Rank
-				oppositeTeam.BasicRank = player.Rank
-			}
-
-			if len(*currentTeam.Players) == 0 && len(*oppositeTeam.Players) != 0 {
-				if math.Abs(float64(oppositeTeam.BasicRank-player.Rank)) > float64(roster.RankRange) {
-					insertedChan <- false
-					continue
-				}
-			}
-
-			if math.Abs(float64(oppositeTeam.BasicRank-player.Rank)) > float64(roster.RankRange) || math.Abs(float64(currentTeam.BasicRank-player.Rank)) > float64(roster.RankRange) {
-				insertedChan <- false
-				continue
-			}
-
-			newPlayers := append(*currentTeam.Players, player)
-			currentTeam.Players = &newPlayers
-
-			if len(*currentTeam.Players) == int(roster.TeamSize) && len(*oppositeTeam.Players) == int(roster.TeamSize) {
+			if finished == true {
 				rosterChan <- roster
-				close(insertedChan)
-				close(rosterChan)
 				break
 			}
 
-			roster.CurrentTeam, roster.OppositeTeam = roster.OppositeTeam, roster.CurrentTeam
-
-			insertedChan <- true
+			insertedChan <- inserted
 			continue
 		}
 
 	}(tb.Roster, tb.insertChan, tb.insertedChan, tb.rosterChan)
+}
+
+func DoBalance(roster *MatchRoster, player Player) (inserted, finished bool) {
+	currentTeam := roster.CurrentTeam
+	oppositeTeam := roster.OppositeTeam
+
+	if len(*currentTeam.Players) == 0 && len(*oppositeTeam.Players) == 0 {
+		currentTeam.BasicRank = player.Rank
+		oppositeTeam.BasicRank = player.Rank
+	}
+
+	if len(*currentTeam.Players) == 0 && len(*oppositeTeam.Players) != 0 {
+		if math.Abs(float64(oppositeTeam.BasicRank-player.Rank)) > float64(roster.RankRange) {
+			return false, false
+		}
+	}
+
+	if math.Abs(float64(oppositeTeam.BasicRank-player.Rank)) > float64(roster.RankRange) || math.Abs(float64(currentTeam.BasicRank-player.Rank)) > float64(roster.RankRange) {
+		return false, false
+	}
+
+	currentTeam.AddPlayer(&player)
+
+	if len(*currentTeam.Players) == int(roster.TeamSize) && len(*oppositeTeam.Players) == int(roster.TeamSize) {
+		return false, true
+	}
+
+	roster.CurrentTeam, roster.OppositeTeam = roster.OppositeTeam, roster.CurrentTeam
+
+	return true, false
 }
--- balancer_test.go
+++ balancer_test.go
@@ -1,15 +1,75 @@
 package main
 
 import (
-	"strconv"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
+func TestTeamBalancer_DoBalancing(t *testing.T) {
+	t.Skip("Unfinished")
 
-
-func TestTeamBalancer_AddPlayer_(t *testing.T) {
 	balancer := TeamBalancer{}
 	balancer.Init(3, 1)
+}
 
-	added, finished := balancer.AddPlayer(Player{"Player" + strconv.Itoa(i + 1), uint8(r1.Intn(6) + 1)})
+type TestResult struct {
+	Inserted,
+	Finished bool
+}
+
+type TestTuple struct {
+	Description    string
+	BaseRoster     *MatchRoster
+	InsertedPlayer *Player
+	Result         TestResult
+}
+
+func provideTestData() []TestTuple {
+	return []TestTuple{
+		{
+			Description: "Empty teams insert one player",
+			BaseRoster:     CreateEmptyRoster(1, 0),
+			InsertedPlayer: CreatePlayer(1),
+			Result: TestResult{
+				Inserted: true,
+				Finished: false,
+			},
+		},
+		{
+			Description: "One full team insert one player with correct rank",
+			BaseRoster: func() *MatchRoster {
+				roster := CreateEmptyRoster(1, 0)
+				roster.TeamA.AddPlayer(CreatePlayer(1))
+				return roster
+			}(),
+			InsertedPlayer: CreatePlayer(1),
+			Result: TestResult{
+				Inserted: true,
+				Finished: true,
+			},
+		}, {
+			Description: "Two full teams insert one player with correct rank",
+			BaseRoster: func() *MatchRoster {
+				roster := CreateEmptyRoster(1, 0)
+				roster.TeamA.AddPlayer(CreatePlayer(1))
+				roster.TeamB.AddPlayer(CreatePlayer(1))
+				return roster
+			}(),
+			InsertedPlayer: CreatePlayer(1),
+			Result: TestResult{
+				Inserted: true,
+				Finished: true,
+			},
+		},
+	}
+}
+
+func TestDoBalance(t *testing.T) {
+	for _, tuple := range provideTestData() {
+		t.Run(tuple.Description, func(t *testing.T) {
+			r := &TestResult{}
+			r.Inserted, r.Finished = DoBalance(tuple.BaseRoster, *tuple.InsertedPlayer)
+			assert.Equal(t, tuple.Result, *r)
+		})
+	}
 }
